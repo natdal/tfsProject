4f535b8550b3606b726cbe714db171a7d3af0f79
natdal <natdal7535@gmail.com>
2014-09-22 17:43:30 +0900

    v.0_map 테스트용 메인뷰딴 가져옴

---
+++ tfsMap/app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def item_info_view
+  end
+end
--- tfsMap/config/routes.rb
+++ tfsMap/config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  get 't_map/index'
+  get 'commerce/sell_and_buy'
 
-  get 'map/index'
+  get 'home/index'
+
+  get 'home/item_info_view'
+
+  get 'home/blog_view'
+
+  get 'home/blog_list'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
